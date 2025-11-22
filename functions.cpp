50e5dc0b25aa8b06f089bba362a17f3a81b0ce10
Austin Pincumbe <apinc@umich.edu>
2025-11-21 23:45:53 -0500

    fixed NewDeck function completed shuffle

--- functions.cpp
+++ functions.cpp
@@ -8,40 +8,52 @@ struct Cards {
     int cardValue;
     std::string suit;
 };
+void ShuffleCards(Cards deck[52]) ;
 
-Cards NewDeck() {
-
-    std::string suitNames[4] = {"Clubs","Diamonds","Hears", "Spades"};
-    int cardsPerSuit[13];
-    Cards deck[52];
-
-    //loop through amount of cards per suit, define amount of cards
-    for (int i =1; i < 14; i++) {
-
-        cardsPerSuit[i] = i;
-
-    }
-
-    for (int i =0; i < 4; i++) {
-
-        for (int j = 1; j < 14; j++) {
-            deck[j].cardValue = j ;
-            deck[j].suit = suitNames[i];
-            std::cout<<deck[j].suit<<std::endl;
-            std::cout<<deck[j].cardValue<<std::endl;
-        }
-
-
-    }
-    return deck[52];
-}
-void shuffleCards() {
+//testing purposes
+void DisplayDeck(Cards deck[52]) {
 
     for (int i =0; i < 51; i++) {
 
+        std::cout<<deck[i].suit<<std::endl;
+        std::cout<<deck[i].cardValue<<std::endl;
 
 
     }
+}
+
+Cards NewDeck() {
+
+    std::string suitNames[4] = {"Clubs","Diamonds","Hearts", "Spades"};
+    Cards deck[52];
+
+    int index=0;
+    for (int i =0; i < 4; i++) {
+
+        for (int j = 1; j < 14; j++) {
+            deck[index].cardValue = j ;
+            deck[index].suit = suitNames[i];
+            index++;
+        }
+
+    }
+    DisplayDeck(deck);
+    ShuffleCards(deck);
+    DisplayDeck(deck);
+
+    return deck[52];
+}
+void ShuffleCards(Cards deck[52]) {
+
+    for (int i =0; i < 51; i++) {
+
+        int random_card = rand() % (1 + i);
+        Cards temp_card = deck[i];
+        deck[i] = deck[random_card];
+        deck[random_card] = temp_card;
+
+    }
+
 
 
 }
