cdf222cf754b152f97a44f3a373deef88381742c
Austin Pincumbe <apinc@umich.edu>
2025-11-22 00:04:06 -0500

    added comments for group

--- functions.cpp
+++ functions.cpp
@@ -26,32 +26,31 @@ void NewDeck(Cards deck[52]) {
 
     std::string suitNames[4] = {"Clubs","Diamonds","Hearts", "Spades"};
 
-
+    //use index for all 52 cards
     int index=0;
+    //loop through suits
     for (int i =0; i < 4; i++) {
 
+        //loop through possible card values
         for (int j = 1; j < 14; j++) {
             deck[index].cardValue = j ;
             deck[index].suit = suitNames[i];
             index++;
         }
-
     }
-
-
 
 }
 void ShuffleCards(Cards deck[52]) {
 
-    for (int i =0; i < 52; i++) {
 
+    for (int i =0; i < 52; i++) {
+        //get random int between 0 and loop count
         int random_card = rand() % (i + 1);
+        //set temp card for sorting
         Cards temp_card = deck[i];
         deck[i] = deck[random_card];
         deck[random_card] = temp_card;
 
     }
-
-
 
 }
